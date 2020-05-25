3a1e0c3d6618b136a8d94f96dab70a2f2ee77252
NickPYTH <nicksrnk@gmail.com>
2020-05-25 18:32:21 +0300

    testim

--- app/Http/Controllers/ShowFrames.php
+++ app/Http/Controllers/ShowFrames.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ShowFrames extends Controller
+class ShowFrames extends Controller #fdfd
 {
     public function FramesFill() {
 
