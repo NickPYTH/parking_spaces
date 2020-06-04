5a889f0b722cbcc2db4de82ac5a3683ad4c608da
NickPYTH <nicksrnk@gmail.com>
2020-06-04 20:50:39 +0300

    added fix images

--- resources/views/inc/frame.blade.php
+++ resources/views/inc/frame.blade.php
@@ -1,6 +1,6 @@
 <div class="card m-5 row">
   <div class="col-12 mt-3">
-  <img class="card-img-top img-thumbnail frame-image" src= "{{ $data[3] }}" alt="Card image cap">
+  <img class="card-img-top" src= "{{ $data[3] }}" alt="Card image cap">
   <div class="card-body">
     <p class="card-text">
     Колличество свободных мест: {{ $data[0] }}<br>
