5e8416a54b9b14e7bb3e32fea553c4ec244e5c56
NickPYTH <nicksrnk@gmail.com>
2020-06-20 01:14:28 +0300

    added about and ask pages final

--- resources/views/ask.blade.php
+++ resources/views/ask.blade.php
@@ -8,8 +8,16 @@ Home
 
 <div class="container">
 
-    Ask
-
+<div class="card m-5 row">
+  <div class="col-12 mt-3 mb-3">
+    <p class="card-text">
+    К сожалению вашего сектора ещё нет в нашем сервисе. Вы можете связаться с нами и добавить его.
+    </p>
+    <div class="row">
+        <div class="col-6 offset-3"><p class="form-control text-center">nicksrnk@gmail.com</p></div>
+    </div>
+  </div>
+</div>
     
 </div>
 @include('inc.footer')
