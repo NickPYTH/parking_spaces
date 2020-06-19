416f114e915f82cc84630bf5ea8b1b7c06c7accc
NickPYTH <nicksrnk@gmail.com>
2020-06-20 00:04:32 +0300

    Added footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="footer">
+<div class="footer">
       <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
+        <span class="text-muted">Привет! Ты смотришь на футер этого сайта)</span>
       </div>
-</footer>
+</div>
